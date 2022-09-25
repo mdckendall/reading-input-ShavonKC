ade22d371d9d43695daace57827c5bd047133f8b
Shavon Campbell <112419578+ShavonKC@users.noreply.github.com>
2022-09-25 19:56:58 +0000

    I read in a word and then i returned the length of it.

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    String word;
+    Scanner s = new Scanner(System.in);
     System.out.println("Enter a word:");
+    word = s.nextLine();
+    int wordSize = word.length();
+    System.out.println("The String has " + wordSize + " characters.");
+    
     
     //Your code will return the length of the entered String
   }
